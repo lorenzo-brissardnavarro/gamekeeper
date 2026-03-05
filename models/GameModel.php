d2b1ab39314de23acd510747fda9a4754b213934
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-03-05 17:54:26 +0100

    feat : méthodes model game

--- models/GameModel.php
+++ models/GameModel.php
@@ -77,11 +77,11 @@ class Game extends Database {
     }
 
     public function getStatus(): string {
-        return $this->image;
+        return $this->status;
     }
 
     public function getStudio(): string {
-        return $this->image;
+        return $this->studio;
     }
 
     public function getIdUser(): int {
@@ -124,6 +124,79 @@ class Game extends Database {
 
     public function setStudio(string $NewStudio): void {
         $this->studio = $NewStudio;
+    }
+
+    // -------------------------------------- Méthodes CRUD ------------------------------------
+
+    // Fonction pour créer un quiz dans BDD
+    public function create(): bool {
+        $sql = "INSERT INTO game (title, description, image, price, release_year, note, duration, favorite, status, studio, id_user) VALUES (:title, :description, :image, :price, :release_year, :note, :duration, :favorite, :status, :studio, :id_user)";
+        $query = $this->pdo->prepare($sql);
+        $success = $query->execute([':title' => $this->title, ':description' => $this->description, ':image' => $this->image, ':price' => $this->price, ':release_year' => $this->release_year, ':note' => $this->note, ':duration' => $this->duration, ':favorite' => $this->favorite, ':status' => $this->status, ':studio' => $this->studio, ':id_user' => $this->id_user]);
+        if ($success) {
+            $this->id = $this->pdo->lastInsertId();
+        }
+        return $success;
+    }
+
+    // Fonction pour modifier un quiz dans BDD
+    public function update(): bool {
+        if (!$this->id) {
+            return false;
+        }
+        $sql = "UPDATE game SET title = :title, description = :description, image = :image, price = :price, release_year = :release_year, note = :note, duration = :duration, favorite = :favorite, status = :status, studio = :studio WHERE id = :id";
+        $query = $this->pdo->prepare($sql);
+        return $query->execute([':title' => $this->title, ':description' => $this->description, ':image' => $this->image, ':price' => $this->price, ':release_year' => $this->release_year, ':note' => $this->note, ':duration' => $this->duration, ':favorite' => $this->favorite, ':status' => $this->status, ':studio' => $this->studio, ':id' => $this->id]);
+    }
+
+    // Fonction pour supprimer un quiz dans BDD
+    public function delete(): bool {
+        if (!$this->id) {
+            return false;
+        }
+        $sql = "DELETE FROM game WHERE id = :id";
+        $query = $this->pdo->prepare($sql);
+        $success = $query->execute([':id' => $this->id]);
+        return $success;
+    }
+
+    // -------------------------------------- Autres méthodes ------------------------------------
+
+    // Fonction pour récupérer tous les jeux de la collection d'un utilisateur
+    public static function getAll(int $id_user): array {
+        $db = new Database();
+        $sql = "SELECT * FROM game WHERE id_user = :id_user";
+        $query = $db->pdo->prepare($sql);
+        $query->execute([':id_user' => $id_user]);
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $games = [];
+        foreach ($results as $row) {
+            $games[] = new Game($row['title'],$row['description'],$row['image'],$row['price'],$row['release_year'],$row['note'],$row['duration'],$row['favorite'],$row['status'],$row['studio'],$row['id_user'],$row['id']);
+        }
+        return $games;
+    }
+
+    //Fonction pour compter le nombre total de jeux appartenant à la collection de l'utilisateur
+    public static function countAll(int $id_user): int {
+        $db = new Database();
+        $sql = "SELECT COUNT(*) AS total FROM game WHERE id_user = :id_user";
+        $query = $db->pdo->prepare($sql);
+        $query->execute([':id_user' => $id_user]);
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        return (int)$result['total'];
+    }
+
+    // Fonction pour récupérer un jeu de la collection de l'utilisateur à partir de son id
+    public static function getById(int $id): ?Game {
+        $db = new Database();
+        $sql = "SELECT * FROM game WHERE id = :id";
+        $query = $db->pdo->prepare($sql);
+        $query->execute([':id' => $id]);
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        if (!$row) {
+            return null;
+        }
+        return new Game($row['title'],$row['description'],$row['image'],$row['price'],$row['release_year'],$row['note'],$row['duration'],$row['favorite'],$row['status'],$row['studio'],$row['id']);
     }
 
 }
